c20f512a6dcc3cae8d90f2de7be3eef227d113ff
Gibran <gibrangul@hotmail.com>
2018-05-29 14:43:53 +0500

    Fixed req typo

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
     // EXPRESS WILL SERVE UP INDEX.HTML IF UNRECOGNIZED URL
     const path = require('path');
-    app.get('*', (res, req) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
